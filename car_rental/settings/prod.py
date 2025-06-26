8d27b530e6a8e7ad48dee4c4a96a07670a3eb0e9
seud <seudsahm1@gmail.com>
2025-06-26 12:49:51 +0300

    chang the url of CORS

--- car_rental/settings/prod.py
+++ car_rental/settings/prod.py
@@ -53,7 +53,7 @@ MEDIA_URL = f"{SUPABASE_URL}/storage/v1/object/public/car-images/"
 # =============
 CORS_ALLOWED_ORIGINS = [
     "https://sudocarreental.netlify.app",
-    "http://127.0.0.1:5501",
+    "https://clever-horse-62e8a4.netlify.app/",
 ] + [f"https://{host}" for host in ALLOWED_HOSTS if host != '*']
 CORS_ALLOW_CREDENTIALS = True
 
